5c77dfe837ba1e9a432778b1ab602d525ac704f5
Caiiru <54562856+Caiiru@users.noreply.github.com>
2023-09-29 18:35:04 -0300

    feat: ship model and water shader
    implementing a ship with all materials and water shader with wave system

---
+++ Assets/Scripts/WaterWaveSystem/ColliderDebug.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderDebug : MonoBehaviour
+{
+    public bool showCollider;
+
+    BoxCollider col;
+
+    public Color color = Color.green;
+
+    public Vector3 v3FrontTopLeft;
+    public Vector3 v3FrontTopRight;
+    public Vector3 v3FrontBottomLeft;
+    public Vector3 v3FrontBottomRight;
+    public Vector3 v3BackTopLeft;
+    public Vector3 v3BackTopRight;
+    public Vector3 v3BackBottomLeft;
+    public Vector3 v3BackBottomRight;
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+
+        v3Center = new Vector3(0, 1.5f, 0f);
+        //v3Extents = new Vector3(3f, 1.25f, 1.5f);
+        v3Extents = col.size / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (showCollider)
+        {
+            CalcPositions();
+            DrawBox();
+
+        }
+    }
+    public Bounds bounds;
+    public Vector3 v3Center;
+    public Vector3 v3Extents;
+
+
+    void CalcPositions()
+    {
+        bounds = col.bounds;
+
+        v3FrontTopLeft = new Vector3(v3Center.x - v3Extents.x, v3Center.y + v3Extents.y, v3Center.z - v3Extents.z);  // Front top left corner
+        v3FrontTopRight = new Vector3(v3Center.x + v3Extents.x, v3Center.y + v3Extents.y, v3Center.z - v3Extents.z);  // Front top right corner
+        v3FrontBottomLeft = new Vector3(v3Center.x - v3Extents.x, v3Center.y - v3Extents.y, v3Center.z - v3Extents.z);  // Front bottom left corner
+        v3FrontBottomRight = new Vector3(v3Center.x + v3Extents.x, v3Center.y - v3Extents.y, v3Center.z - v3Extents.z);  // Front bottom right corner
+        v3BackTopLeft = new Vector3(v3Center.x - v3Extents.x, v3Center.y + v3Extents.y, v3Center.z + v3Extents.z);  // Back top left corner
+        v3BackTopRight = new Vector3(v3Center.x + v3Extents.x, v3Center.y + v3Extents.y, v3Center.z + v3Extents.z);  // Back top right corner
+        v3BackBottomLeft = new Vector3(v3Center.x - v3Extents.x, v3Center.y - v3Extents.y, v3Center.z + v3Extents.z);  // Back bottom left corner
+        v3BackBottomRight = new Vector3(v3Center.x + v3Extents.x, v3Center.y - v3Extents.y, v3Center.z + v3Extents.z);  // Back bottom right corner
+
+        v3FrontTopLeft = transform.TransformPoint(v3FrontTopLeft);
+        v3FrontTopRight = transform.TransformPoint(v3FrontTopRight);
+        v3FrontBottomLeft = transform.TransformPoint(v3FrontBottomLeft);
+        v3FrontBottomRight = transform.TransformPoint(v3FrontBottomRight);
+        v3BackTopLeft = transform.TransformPoint(v3BackTopLeft);
+        v3BackTopRight = transform.TransformPoint(v3BackTopRight);
+        v3BackBottomLeft = transform.TransformPoint(v3BackBottomLeft);
+        v3BackBottomRight = transform.TransformPoint(v3BackBottomRight);
+
+    }
+    void DrawBox()
+    {
+        Debug.DrawLine(v3FrontTopLeft, v3FrontTopRight, color);
+        Debug.DrawLine(v3FrontTopRight, v3FrontBottomRight, color);
+        Debug.DrawLine(v3FrontBottomRight, v3FrontBottomLeft, color);
+        Debug.DrawLine(v3FrontBottomLeft, v3FrontTopLeft, color);
+
+        Debug.DrawLine(v3BackTopLeft, v3BackTopRight, color);
+        Debug.DrawLine(v3BackTopRight, v3BackBottomRight, color);
+        Debug.DrawLine(v3BackBottomRight, v3BackBottomLeft, color);
+        Debug.DrawLine(v3BackBottomLeft, v3BackTopLeft, color);
+
+        Debug.DrawLine(v3FrontTopLeft, v3BackTopLeft, color);
+        Debug.DrawLine(v3FrontTopRight, v3BackTopRight, color);
+        Debug.DrawLine(v3FrontBottomRight, v3BackBottomRight, color);
+        Debug.DrawLine(v3FrontBottomLeft, v3BackBottomLeft, color);
+
+    }
+
+
+
+}
